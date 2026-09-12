c1bb1250c3f3bfa3eeddf9be54846a37258a1356
aminur <aminur6264@gmail.com>
2026-09-12 18:37:20 +0600

    Initial Commit

---
+++ Forever0507Solution/Forever0507App/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Forever0507App.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Forever0507App.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Forever0507Solution/Forever0507App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Forever0507App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
